e6abbef4e3027fa305a26f11fe10a9987d0203f7
Alexandra Moore <alexmoore94@hotmail.co.uk>
2017-10-05 16:58:28 +0100

    increment/decrement buttons

--- Home.elm
+++ Home.elm
@@ -1,11 +1,39 @@
 module ElmQuiz exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
+main = 
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+
+model = 
+    0
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model = 
+    case msg of
+        Increment ->
+            model + 1
+        Decrement ->
+            model - 1
+
+-- VIEW
+
+view : Model -> Html Msg
 view model =
-        h1 []
-            [text "Pub Quiz"]
+    div []
+        [ button [ onClick Decrement] [ text "-"]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
 
-main =
-    view "dummy model"
